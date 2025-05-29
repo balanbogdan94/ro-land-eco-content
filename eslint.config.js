0bda991452e057b1af2137bdad05d5dcc8dcd676
balanbogdan94-pc <balan.bogdan94@gmail.com>
2025-05-29 23:00:26 +0300

    chore: add eslint-plugin-import for improved import management
    refactor: clean up import statements in App.tsx for consistency

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,44 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import importPlugin from 'eslint-plugin-import';
 
-export default tseslint.config(
-  { ignores: ["dist"] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+export default tseslint.config({
+  ignores: ['dist'],
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', path.resolve(__dirname, 'src')]],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
     },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+  },
+
+  extends: [js.configs.recommended, ...tseslint.configs.recommended],
+  files: ['**/*.{ts,tsx}'],
+  languageOptions: {
+    ecmaVersion: 'latest',
+    globals: {
+      window: 'readonly',
+      document: 'readonly',
+      navigator: 'readonly',
     },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-      "@typescript-eslint/no-unused-vars": "off",
-    },
-  }
-);
+  },
+  plugins: {
+    'import-plugin': importPlugin,
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+  },
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-unused-vars': 'error',
+    'max-lines': ['error', { max: 150, skipComments: true }],
+    'max-lines-per-function': ['error', { max: 50, skipComments: true }],
+    'max-params': ['error', { max: 5 }],
+    'import-plugin/no-unresolved': 'error',
+    'import-plugin/no-default-export': 'error',
+  },
+});
